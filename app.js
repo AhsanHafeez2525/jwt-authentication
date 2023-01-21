45a2040c601876e95c82cda6f56f9f39abd9d5c9
Ahsan <ahsansatti402@gmail.com>
2023-01-22 01:05:11 +0500

    complete

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+const secretKey = "secretKey";
+
+
+app.get('/', (req,res) =>{
+    res.json({
+        message: "Ahsan"
+    })
+})
+
+app.post('/login',(req,res)=>{
+
+    const user={
+        id: 1,
+        username: "ahsan hafeez",
+        email: "ahsansatti402@gmail.com"
+    }
+    jwt.sign((user), secretKey, {expiresIn: '300s'}, (err, token)=>{
+        res.json({
+            token
+        })
+    })
+})
+
+app.post("/profile", verifyToken,(req, res)=>{
+    jwt.verify(req.token,secretKey,(err,authData)=>{
+        if(err){
+           res.send({result: "invalid token"}) 
+        }
+        else{
+            res.json({
+                message: "profile accessed",
+                authData
+            })
+        }
+    })
+
+})
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader !== 'undefined'){
+const bearer = bearerHeader.split(" ");
+const token = bearer[1];
+req.token=token;
+next();
+    }
+    else{
+        res.send({
+            result: 'Token is not valid'
+        })
+    }
+}
+
+app.listen(5000,()=>{
+    console.log("server running")
+})
